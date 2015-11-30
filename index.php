f3bc7a9a646780cd5213e97e9d4ec38722c1dc11
Antony Manrique <antony476@gmail.com>
2015-11-30 09:34:10 -0500

    subiendo ejemplo

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>adasdad</title>
+	<!-- Latest compiled and minified CSS & JS -->
+	<link rel="stylesheet" media="screen" href="//netdna.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="//netdna.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<h1>asdasdasdasdasd</h1>
+</body>
+</html>
